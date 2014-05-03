553dd3cf3730d7c6b3fe7e9f52fe18d58688903e
Sebastian Schmidt <isibboi@gmail.com>
2014-05-03 23:11:50 +0200

    Corrected all remaining checkstyle errors.

--- src/test/java/de/isibboi/agentsim/noise/RangeTest.java
+++ src/test/java/de/isibboi/agentsim/noise/RangeTest.java
@@ -4,22 +4,33 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+/**
+ * Checks if the noise functions return only values between their global minimum and maximum.
+ * @author Sebastian Schmidt
+ * @since 0.0.0
+ */
 public class RangeTest {
-	private static final double[][] testRanges = new double[][] { { 0, 100 }, { 200, 201 }, { -100, 0 }, { -100, 100 } };
+	private static final double[][] TEST_RANGES = new double[][] { { 0, 100 }, { 200, 201 }, { -100, 0 }, { -100, 100 } };
 
+	/**
+	 * Tests if the scaled simplex noise function returns values in the correct range.
+	 */
 	@Test
-	public void testNoiseMapRange() {
-		for (int i = 0; i < testRanges.length; i++) {
-			ScaledSimplexNoise noiseMap = new ScaledSimplexNoise(1, 1, testRanges[i][0], testRanges[i][1]);
+	public void testScaledSimplexNoiseRange() {
+		for (int i = 0; i < TEST_RANGES.length; i++) {
+			ScaledSimplexNoise noiseMap = new ScaledSimplexNoise(1, 1, TEST_RANGES[i][0], TEST_RANGES[i][1]);
 
 			for (int j = 0; j < 10000; j++) {
 				double sample = noiseMap.noise(j % 100, j / 100);
 
-				assertRange(testRanges[i], sample);
+				assertRange(TEST_RANGES[i], sample);
 			}
 		}
 	}
 
+	/**
+	 * Checks if the simplex noise function returns values in the correct range.
+	 */
 	@Test
 	public void testSimplexNoiseRange() {
 		SimplexNoise noise = new SimplexNoise(49805643105274513L);
@@ -30,10 +41,21 @@ public class RangeTest {
 		}
 	}
 
+	/**
+	 * Asserts that the given sample is in the given range.
+	 * @param range The range. {@code range[0]} is the minimum, {@code range[1]} is the maximum.
+	 * @param sample The sample.
+	 */
 	private void assertRange(final double[] range, final double sample) {
 		assertRange(range[0], range[1], sample);
 	}
 
+	/**
+	 * Asserts that the given sample is in the given range.
+	 * @param min The lower bound of the range.
+	 * @param max The upper bound of the range.
+	 * @param sample The sample.
+	 */
 	private void assertRange(final double min, final double max, final double sample) {
 		assertTrue("Sample out of range [" + min + ", " + max + "]: " + sample, sample >= min && sample <= max);
 	}
