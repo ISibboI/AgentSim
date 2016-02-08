a1ca7c0a3cf12f38818ac2a3010e35a4d7e0d097
Sebastian Schmidt <isibboi@gmail.com>
2016-02-08 15:42:00 +0100

    Made run.sh give some informative output

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "Updating application"
 git pull
+echo "Building application"
 mvn -q -DskipTests package
+echo "Running application"
 java -jar target/Goblins-jar-with-dependencies.jar
+echo "JVM terminated"
