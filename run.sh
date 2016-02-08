c5e7803f569b739b423577f6d575fa7dc801de31
Sebastian Schmidt <isibboi@gmail.com>
2016-02-08 15:43:13 +0100

    Made output a little bit nicer

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-echo "Updating application"
+echo "Updating application..."
 git pull
-echo "Building application"
+echo "Done."
+echo "Building application..."
 mvn -q -DskipTests package
-echo "Running application"
+echo "Done."
+echo "Running application..."
 java -jar target/Goblins-jar-with-dependencies.jar
-echo "JVM terminated"
+echo "JVM terminated."
