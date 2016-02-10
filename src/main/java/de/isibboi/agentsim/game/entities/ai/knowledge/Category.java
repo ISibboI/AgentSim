f2bae342b25688bddbe4afa8a84b221a2eccf6ec
Sebastian Schmidt <isibboi@gmail.com>
2016-02-10 16:04:25 +0100

    Working on category group creation.

--- src/main/java/de/isibboi/agentsim/game/entities/ai/knowledge/Category.java
+++ src/main/java/de/isibboi/agentsim/game/entities/ai/knowledge/Category.java
@@ -1,11 +1,101 @@
 package de.isibboi.agentsim.game.entities.ai.knowledge;
 
+import java.util.Objects;
+
 /**
  * A category an object may belong to.
+ * A category has a name and an id, and always belongs to a category group.
+ * The name can be any String.
+ * The id is a number given by the {@link CategoryGroup}, used as an index for {@link CategorySet} data structures.
  * 
  * @author Sebastian Schmidt
  * @since 0.3.0
  */
-public class Category {
+public final class Category {
+	/**
+	 * Builds a new category.
+	 * 
+	 * @author Sebastian Schmidt
+	 * @since 0.3.0
+	 */
+	public static class Builder {
+		private String _name;
 
+		/**
+		 * Creates a new category builder with the given category name.
+		 * 
+		 * @param name The name of the category to be built.
+		 */
+		public Builder(final String name) {
+			_name = name;
+		}
+
+		/**
+		 * Creates a new category.
+		 * 
+		 * @param id The id of the category.
+		 * @param categoryGroup The group the category belongs to.
+		 * @return A new category.
+		 */
+		Category build(final int id, final CategoryGroup categoryGroup) {
+			Objects.requireNonNull(_name);
+			Objects.requireNonNull(categoryGroup);
+			return new Category(_name, id, categoryGroup);
+		}
+
+		@Override
+		public boolean equals(final Object o) {
+			if (o instanceof Builder) {
+				Builder b = (Builder) o;
+
+				if (_name == null || b._name == null) {
+					return _name == b._name;
+				}
+
+				return b._name.equals(_name);
+			} else {
+				return false;
+			}
+		}
+
+		@Override
+		public int hashCode() {
+			if (_name == null) {
+				return 0;
+			} else {
+				return _name.hashCode();
+			}
+		}
+	}
+
+	private final String _name;
+	private final int _id;
+	private final CategoryGroup _categoryGroup;
+
+	/**
+	 * Creates a new {@link Category} with the given name and id.
+	 * @param name The name of the category.
+	 * @param id The id of the category.
+	 * @param categoryGroup The {@link CategoryGroup} this category belongs to.
+	 */
+	private Category(final String name, final int id, final CategoryGroup categoryGroup) {
+		_name = name;
+		_id = id;
+		_categoryGroup = categoryGroup;
+	}
+
+	@Override
+	public boolean equals(final Object o) {
+		if (o instanceof Category) {
+			Category c = (Category) o;
+			return c._name.equals(_name) && c._categoryGroup.equals(_categoryGroup);
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return _name.hashCode() ^ _id ^ _categoryGroup.hashCode();
+	}
 }
--- src/main/java/de/isibboi/agentsim/game/entities/ai/knowledge/CategoryGroup.java
+++ src/main/java/de/isibboi/agentsim/game/entities/ai/knowledge/CategoryGroup.java
@@ -1,6 +1,6 @@
 package de.isibboi.agentsim.game.entities.ai.knowledge;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collection;
 
 /**
@@ -9,27 +9,62 @@ import java.util.Collection;
  * @author Sebastian Schmidt
  * @since 0.3.0
  */
-public class CategoryGroup {
+public final class CategoryGroup {
+	/**
+	 * Builds a new category group.
+	 * 
+	 * @author Sebastian Schmidt
+	 * @since 0.3.0
+	 */
+	public static class Builder {
+		private final Collection<Category.Builder> _categories = new ArrayList<>();
+
+		/**
+		 * Creates a new empty builder.
+		 */
+		public Builder() {
+		}
+
+		/**
+		 * Adds a category with the given name to this category group builder.
+		 * @param name The name of the category.
+		 * @return True if the category builder changed as a result of the call.
+		 */
+		public boolean add(final String name) {
+			return add(new Category.Builder(name));
+		}
+
+		/**
+		 * Adds the given category builder to this category group builder.
+		 * @param category The category builder.
+		 * @return True if the category builder changed as a result of the call.
+		 */
+		public boolean add(final Category.Builder category) {
+			return _categories.add(category);
+		}
+
+		/**
+		 * Creates a new {@link CategoryGroup} from this builder.
+		 * @return A new {@link CategoryGroup}.
+		 */
+		public CategoryGroup build() {
+			return new CategoryGroup(_categories);
+		}
+	}
+
 	private final Category[] _categories;
 
 	/**
 	 * Creates a new category group with the given categories.
 	 * @param categories The categories.
 	 */
-	public CategoryGroup(final Collection<Category> categories) {
+	private CategoryGroup(final Collection<Category.Builder> categories) {
 		_categories = new Category[categories.size()];
 
 		int i = 0;
-		for (Category category : categories) {
-			_categories[i++] = category;
+		for (Category.Builder category : categories) {
+			_categories[i] = category.build(i, this);
+			i++;
 		}
-	}
-
-	/**
-	 * Creates a new category group with the given categories.
-	 * @param categories The categories.
-	 */
-	public CategoryGroup(final Category[] categories) {
-		_categories = Arrays.copyOf(categories, categories.length);
 	}
 }
